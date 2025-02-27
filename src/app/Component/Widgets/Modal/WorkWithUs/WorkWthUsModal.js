69bb930e9fd1362a4e6cf2a8088682c3f057dc43
Sushobhan Dastidar <sushobhan@reddensoft.com>
2025-02-27 17:24:21 +0530

    Frontend: modal cross fixed

--- src/app/Component/Widgets/Modal/WorkWithUs/WorkWthUsModal.js
+++ src/app/Component/Widgets/Modal/WorkWithUs/WorkWthUsModal.js
@@ -50,7 +50,7 @@ const WorkWthUsModal = ({ isOpen, onClose }) => {
     <div id="modal" className='fixed top-0 left-0 w-screen h-screen bg-black bg-opacity-50 overflow-auto z-[9999] px-2'>
       <div className='modal-centered flex items-center'>
         <div className='container mx-auto relative xl:max-w-[1000px] bg-[#f8f8f8] border-[1px] border-solid border-[#00000033] rounded-[10px] !p-[10px] md:p-[16px]'>
-          <button onClick={onClose} className='text-[16px] text-[#666] font-bold absolute top-[10px] right-[10px]'>X</button>
+          <button onClick={onClose} className='text-[16px] text-[#666] font-bold absolute top-[10px] right-[10px] size-[30px] border border-[#ddd] rounded-full'>X</button>
           <div className='max-w-[100%] lg:max-w-[90%] m-auto'>
             <div className='py-[20px] xxl:py-[32px]'>
               <h2 className='text-[20px] md:text-[26px] lg:text-[30px] font-bold leading-[1.25] text-black text-center'>
