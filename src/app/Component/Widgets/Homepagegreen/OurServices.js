0e85dd5b03ae803d32d94930ed712a9de9b0ca26
Sushobhan Dastidar <sushobhan@reddensoft.com>
2025-02-28 16:36:16 +0530

    Frontend: learnmorebtn removed workwithus

--- src/app/Component/Widgets/Homepagegreen/OurServices.js
+++ src/app/Component/Widgets/Homepagegreen/OurServices.js
@@ -26,66 +26,66 @@ export default function OurServices()  {
                             <div className="h-full text-center p-[15px] lg:p-[20px] xl:p-[30px] pb-[60px] bg-[#292929] rounded-[10px] relative">
                                 <Image src={SearchEngineOptimization} alt='Service-Icon-one' className="max-h-[80px] lg:max-h-[100px] m-auto object-contain" />
                                 <h2 className="mt-[30px] text-[20px] xl:text-[22px] text-white leading-[1.26] font-bold text-center">Search Engine Optimization</h2>
-                                <p className="text-[15px] text-[#bcbcbc] leading-[1.39] mt-[15px]">Our expert SEO team and content writers will craft conversion-driven <strong>digital marketing services</strong> that leverages years of SEO consultancy and strategy implementation to ensure your business ranks higher on Google.</p>
-                                {/* <Link href="#" className="absolute left-0 right-0 bottom-[25px] flex justify-center text-[15px] text-[#5db947] leading-[2.13] font-bold hover:underline m-auto w-fit">
+                                <p className="text-[15px] text-[#bcbcbc] leading-[1.39] mt-[15px] pb-[40px]">Our expert SEO team and content writers will craft conversion-driven <strong>digital marketing services</strong> that leverages years of SEO consultancy and strategy implementation to ensure your business ranks higher on Google.</p>
+                                <Link href="/services/search-engine-optimization" className="absolute left-0 right-0 bottom-[25px] flex justify-center text-[15px] text-[#5db947] leading-[2.13] font-bold hover:underline m-auto w-fit">
                                     Learn more 
                                     <Image src={CtaSmall} alt='cta-small' className="ml-[10px]" />
-                                </Link> */}
+                                </Link>
                             </div>
                         </div>
                         <div className="lg:w-4/12 md:w-6/12 w-full p-[4px] md:p-[6px] relative">
                             <div className="h-full text-center p-[15px] lg:p-[20px] xl:p-[30px] pb-[60px] bg-[#292929] rounded-[10px] relative">
                                 <Image src={PaidAdvertising} alt='Service-Icon-one' className="max-h-[80px] lg:max-h-[100px] m-auto object-contain" />
                                 <h2 className="mt-[30px] text-[20px] xl:text-[22px] text-white leading-[1.26] font-bold text-center">Paid Advertising</h2>
-                                <p className="text-[15px] text-[#bcbcbc] leading-[1.39] mt-[15px]">From Google Ads to social media advertising, we create highly targeted paid campaigns that deliver measurable results. Data-driven approach at our <strong>online marketing agency</strong> ensures that your ad spend is optimized for maximum return on investment.</p>
-                                {/* <Link href="#" className="absolute left-0 right-0 bottom-[25px] flex justify-center text-[15px] text-[#5db947] leading-[2.13] font-bold hover:underline m-auto w-fit">
+                                <p className="text-[15px] text-[#bcbcbc] leading-[1.39] mt-[15px] pb-[40px]">From Google Ads to social media advertising, we create highly targeted paid campaigns that deliver measurable results. Data-driven approach at our <strong>online marketing agency</strong> ensures that your ad spend is optimized for maximum return on investment.</p>
+                                <Link href="/services/google-ads" className="absolute left-0 right-0 bottom-[25px] flex justify-center text-[15px] text-[#5db947] leading-[2.13] font-bold hover:underline m-auto w-fit">
                                     Learn more 
                                     <Image src={CtaSmall} alt='cta-small' className="ml-[10px]" />
-                                </Link> */}
+                                </Link>
                             </div>
                         </div>
                         <div className="lg:w-4/12 md:w-6/12 w-full p-[4px] md:p-[6px] relative">
                             <div className="h-full text-center p-[15px] lg:p-[20px] xl:p-[30px] pb-[60px] bg-[#292929] rounded-[10px] relative">
                                 <Image src={SocialMediaMarketing} alt='Service-Icon-one' className="max-h-[80px] lg:max-h-[100px] m-auto object-contain" />
                                 <h2 className="mt-[30px] text-[20px] xl:text-[22px] text-white leading-[1.26] font-bold text-center">Social Media Marketing</h2>
-                                <p className="text-[15px] text-[#bcbcbc] leading-[1.39] mt-[15px]">We design and execute impactful social media campaigns that drive engagement, build brand awareness, and ultimately lead to higher conversions. Strategic approach in our <strong>internet marketing agency</strong> will help you connect with your audience in meaningful ways. </p>
-                                {/* <Link href="#" className="absolute left-0 right-0 bottom-[25px] flex justify-center text-[15px] text-[#5db947] leading-[2.13] font-bold hover:underline m-auto w-fit">
+                                <p className="text-[15px] text-[#bcbcbc] leading-[1.39] mt-[15px] pb-[30px]">We design and execute impactful social media campaigns that drive engagement, build brand awareness, and ultimately lead to higher conversions. Strategic approach in our <strong>internet marketing agency</strong> will help you connect with your audience in meaningful ways. </p>
+                                <Link href="/services/social-media-marketing" className="absolute left-0 right-0 bottom-[25px] flex justify-center text-[15px] text-[#5db947] leading-[2.13] font-bold hover:underline m-auto w-fit">
                                     Learn more 
                                     <Image src={CtaSmall} alt='cta-small' className="ml-[10px]" />
-                                </Link> */}
+                                </Link>
                             </div>
                         </div>
                         <div className="lg:w-4/12 md:w-6/12 w-full p-[4px] md:p-[6px] relative">
                             <div className="h-full text-center p-[15px] lg:p-[20px] xl:p-[30px] sm:pb-[60px] bg-[#292929] rounded-[10px] relative">
                                 <Image src={EmailMarketing} alt='Service-Icon-one' className="max-h-[80px] lg:max-h-[100px] m-auto object-contain" />
                                 <h2 className="mt-[30px] text-[20px] xl:text-[22px] text-white leading-[1.26] font-bold text-center">Email Marketing</h2>
-                                <p className="text-[15px] text-[#bcbcbc] leading-[1.39] mt-[15px]">Our email marketing experts create personalized, conversion-focused campaigns to help you nurture leads and retain customers. From segmented lists to automated workflows, we ensure your emails drive results.</p>
-                                {/* <Link href="#" className="absolute left-0 right-0 bottom-[25px] flex justify-center text-[15px] text-[#5db947] leading-[2.13] font-bold hover:underline m-auto w-fit">
+                                <p className="text-[15px] text-[#bcbcbc] leading-[1.39] mt-[15px] pb-[30px]">Our email marketing experts create personalized, conversion-focused campaigns to help you nurture leads and retain customers. From segmented lists to automated workflows, we ensure your emails drive results.</p>
+                                <Link href="/services/email-marketing" className="absolute left-0 right-0 bottom-[25px] flex justify-center text-[15px] text-[#5db947] leading-[2.13] font-bold hover:underline m-auto w-fit">
                                     Learn more 
                                     <Image src={CtaSmall} alt='cta-small' className="ml-[10px]" />
-                                </Link> */}
+                                </Link>
                             </div>
                         </div>
                         <div className="lg:w-4/12 md:w-6/12 w-full p-[4px] md:p-[6px] relative">
                             <div className="h-full text-center p-[15px] lg:p-[20px] xl:p-[30px] sm:pb-[60px] bg-[#292929] rounded-[10px] relative">
                                 <Image src={ContentMarketingDd} alt='Service-Icon-one' className="max-h-[80px] lg:max-h-[100px] m-auto object-contain" />
                                 <h2 className="mt-[30px] text-[20px] xl:text-[22px] text-white leading-[1.26] font-bold text-center">Content Marketing</h2>
-                                <p className="text-[15px] text-[#bcbcbc] leading-[1.39] mt-[15px]">Our content writers produce high-quality, SEO-optimized content that not only attracts traffic but also converts visitors into loyal customers. Our <strong>digital marketing solution</strong> develops a strategy that resonates with your target audience and supports your overall marketing goals.</p>
-                                {/* <Link href="#" className="absolute left-0 right-0 bottom-[25px] flex justify-center text-[15px] text-[#5db947] leading-[2.13] font-bold hover:underline m-auto w-fit">
+                                <p className="text-[15px] text-[#bcbcbc] leading-[1.39] mt-[15px] pb-[30px]">Our content writers produce high-quality, SEO-optimized content that not only attracts traffic but also converts visitors into loyal customers. Our <strong>digital marketing solution</strong> develops a strategy that resonates with your target audience and supports your overall marketing goals.</p>
+                                <Link href="/services/content-marketing" className="absolute left-0 right-0 bottom-[25px] flex justify-center text-[15px] text-[#5db947] leading-[2.13] font-bold hover:underline m-auto w-fit">
                                     Learn more 
                                     <Image src={CtaSmall} alt='cta-small' className="ml-[10px]" />
-                                </Link> */}
+                                </Link>
                             </div>
                         </div>
                     </div>
-                    <div className="text-center">
+                    {/* <div className="text-center">
                         <button onClick={openModal} className="ease_in_out bg-[#5db947] text-[14px] md:text-[15px] font-bold text-white uppercase inline-flex border border-solid border-[#5db947] px-[30px] md:px-[55px] py-[15px] md:py-[20px] mt-[40px] hover:bg-[#5db947]">
                             Work With Us
                             <span className="ml-[10px]">
                                 <Image src={BtnArrow} alt="arrow" />
                             </span>
                         </button>
-                    </div>
+                    </div> */}
                 </div>
             </div>
             <WorkWthUsModal isOpen={isModalOpen} onClose={closeModal} />
