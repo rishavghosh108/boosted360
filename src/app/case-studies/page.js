7c4b1f12ba16472e904207ee4da365d818f86c09
Sushobhan Dastidar <sushobhan@reddensoft.com>
2025-03-10 13:34:37 +0530

    Case studies res

--- src/app/case-studies/page.js
+++ src/app/case-studies/page.js
@@ -4,7 +4,8 @@ import Header from '../Component/Layout/Header';
 import { work1, work2, work3, work4, work5, work6 } from '../assets/index';
 import Link from 'next/link';
 import Image from 'next/image';
-import FooterGreen from '../Component/Layout/Footer';
+import { Layouts } from '@/app/Component';
+import Growing from '../Component/Widgets/Homepagegreen/Growing';
 
 const page = () => {
 
@@ -41,20 +42,18 @@ const page = () => {
         },
     ]
     return (
-        <>
-            <Header />
-
-            <div className='pt-[120px] pb-[55px]'>
+        <Layouts>
+            <div className='pt-8 pb-16'>
                 <div className='container'>
-                    <div className='grid grid-cols-2 gap-[25px]'>
+                    <div className='grid grid-cols-1 lg:grid-cols-2 gap-[18px] sm:gap-[25px] lg:px-0 px-0 sm:px-10 nn1:px-[15%]'>
                         {SlideItems.map((item, index) => (
-                            <div key={index} className='bg-blue-950 h-[350px] hover:shadow-md rounded-xl overflow-hidden relative group'>
-                                <div className='w-[48%] rounded-xl p-5 xl:p-6 text-white text-left h-full flex flex-col justify-center'>
-                                    <h3 className="text-xl xl:text-2xl 2xl:text-3xl font-semibold pb-3 xl:pb-4">{item.title}</h3>
-                                    <p className="text-sm xl:text-base xl:leading-5 font-light pb-4">{item.description}</p>
+                            <div key={index} className='bg-blue-950 h-[188px] sm:h-[300px] md:h-[360px] lg:h-[310px] xl:h-[350px] hover:shadow-md rounded-xl overflow-hidden relative group'>
+                                <div className='w-[48%] rounded-xl p-2 sm:p-5 xl:p-6 text-white text-left h-full flex flex-col justify-center'>
+                                    <h3 className="text-[16px] sm:text-xl xl:text-2xl 2xl:text-3xl font-semibold pb-3 xl:pb-4">{item.title}</h3>
+                                    <p className="text-[12px] sm:text-sm xl:text-base leading-[1] xl:leading-5 font-light pb-4">{item.description}</p>
                                 </div>
 
-                                <Link class="absolute w-14 h-14 left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 scale-0 group-hover:scale-100 transition-all duration-300 ease-in-out rounded-full bg-white flex items-center justify-center z-[9] shadow-lg" href="#">
+                                <Link class="absolute size-[2rem] sm:size-14 left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 scale-0 group-hover:scale-100 transition-all duration-300 ease-in-out rounded-full bg-white flex items-center justify-center z-[9] shadow-lg" href="#">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" class="fill-secondary" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M14 2.5a.5.5 0 0 0-.5-.5h-6a.5.5 0 0 0 0 1h4.793L2.146 13.146a.5.5 0 0 0 .708.708L13 3.707V8.5a.5.5 0 0 0 1 0z"></path></svg>
                                 </Link>
                                 <div className='absolute top-0 right-0 bottom-0 left-0 size-full rounded-xl group-hover:left-[50%] transition-all ease-in duration-300'>
@@ -66,9 +65,8 @@ const page = () => {
                     </div>
                 </div>
             </div>
-
-            <FooterGreen />
-        </>
+            <Growing/>
+        </Layouts>
     )
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,6 +9,7 @@ module.exports = {
     screens: {
       sm: '576px',
       md: '768px',
+      'nn1':'991px',
       lg: '1024px',
       xl: '1280px',
       xxl: '1536px',
