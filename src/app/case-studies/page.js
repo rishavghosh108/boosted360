9157c436e10f8f6d0147ef31b8317e60d0ca6df4
Sushobhan Dastidar <sushobhan@reddensoft.com>
2025-03-10 15:14:15 +0530

    Case studies listing fix

--- src/app/case-studies/page.js
+++ src/app/case-studies/page.js
@@ -47,7 +47,7 @@ const page = () => {
                 <div className='container'>
                     <div className='grid grid-cols-1 lg:grid-cols-2 gap-[18px] sm:gap-[25px] lg:px-0 px-0 sm:px-10 nn1:px-[15%]'>
                         {SlideItems.map((item, index) => (
-                            <div key={index} className='bg-blue-950 h-full min-h-[182px] max-h-[336px] hover:shadow-md rounded-xl overflow-hidden relative group'>
+                            <div key={index} className='bg-blue-950 hover:shadow-md rounded-xl overflow-hidden relative group'>
                                 <div className='w-[48%] absolute top-0 left-0 rounded-xl p-2 sm:p-5 xl:p-6 text-white text-left h-full flex flex-col justify-center'>
                                     <h3 className="text-[16px] sm:text-xl xl:text-2xl 2xl:text-3xl font-semibold pb-3 xl:pb-4">{item.title}</h3>
                                     <p className="text-[12px] sm:text-sm xl:text-base leading-[1] xl:leading-5 font-light pb-4">{item.description}</p>
@@ -56,16 +56,14 @@ const page = () => {
                                 <Link className="absolute size-[2rem] sm:size-14 left-1/2 top-1/2 transform -translate-x-1/2 -translate-y-1/2 scale-0 group-hover:scale-100 transition-all duration-300 ease-in-out rounded-full bg-white flex items-center justify-center z-[9] shadow-lg" href="#">
                                     <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" class="fill-secondary" viewBox="0 0 16 16"><path fill-rule="evenodd" d="M14 2.5a.5.5 0 0 0-.5-.5h-6a.5.5 0 0 0 0 1h4.793L2.146 13.146a.5.5 0 0 0 .708.708L13 3.707V8.5a.5.5 0 0 0 1 0z"></path></svg>
                                 </Link>
-                                <div className='relative top-0 bottom-0 left-0 size-full rounded-xl group-hover:left-[50%] transition-all ease-in duration-300'>
-                                    <Image className='size-full object-cover rounded-xl' src={item.imageUrl} alt={`${item.imageUrl.src}`} />
-                                </div>
+                                <Image className='size-full object-cover rounded-xl relative left-0 group-hover:left-[50%] transition-all ease-in duration-300' src={item.imageUrl} alt={`${item.imageUrl.src}`} />
                             </div>
                         ))}
 
                     </div>
                 </div>
             </div>
-            <Growing/>
+            <Growing />
         </Layouts>
     )
 }
