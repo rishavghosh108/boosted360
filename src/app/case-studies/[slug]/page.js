60043aed5389a4c9f33c7d970b30a790d650f4a4
Sushobhan Dastidar <sushobhan@reddensoft.com>
2025-03-11 14:55:56 +0530

    detals-page content update

--- src/app/case-studies/[slug]/page.js
+++ src/app/case-studies/[slug]/page.js
@@ -23,13 +23,15 @@ const Page = () => {
               <Image src={'/images/details-image/curbappeal.png'} width={100} height={100} alt={`Slide`} className='w-40 mx-auto lg:mx-0 brightness-75' />
 
               <h1 className='lg:text-5xl md:text-4xl text-3xl text-neutral-800 md:pt-10 pt-5 font-light text-center lg:text-left'>
-                Empowering Anthem Senior
+                Empowering Curb Appeal Lawn&apos;s 
                 {/* <br className='hidden lg:block' /> */}
-                <span className='font-bold'>Living&apos;s Digital Growth</span>
+                <span className='font-bold'> Digital Transformation</span>
               </h1>
 
               <p className='pt-5 md:text-xl text-base text-neutral-800 text-center lg:text-left'>
-                Boosted 360 enhanced Anthem Senior Living&apos;s <span className='font-bold text-[#459231]'>digital marketing</span> with targeted content, social media, and SEO, boosting visibility and <span className='font-bold text-[#459231]'>driving growth</span>.
+                
+
+                Boosted 360 drove growth for Curb Appeal Lawn through a <span className='font-bold text-[#459231]'>digital marketing</span> strategy that included SEO, content marketing, resulting in increased brand <span className='font-bold text-[#459231]'>visibility, engagement, and revenue.</span> 
 
               </p>
               <button onClick={openModal} className="ease_in_out box-shadows bg-[#2a2a2a] text-[14px] md:text-[15px] font-bold text-white uppercase flex w-fit border border-solid border-[#3e3e3e] px-[30px] md:px-[55px] py-[15px] md:py-[20px] lg:mx-0 mx-auto mt-[40px] hover:bg-[#404040] hover:border-[#404040]">
@@ -51,7 +53,7 @@ const Page = () => {
         <div className='grid md:grid-cols-3 lg:gap-8 md:gap-5 gap-3 text-center items-center pb-10 lg:pt-5'>
           <div className='bg-white border py-10 px-2'>
             <h2 className='text-[#459231] lg:text-6xl md:text-5xl sm:text-4xl text-3xl font-light'>
-              1.8k
+            5k/<sub className='text-[22px] font-semibold relative bottom-0'>month</sub>
             </h2>
             <p className='text-neutral-700 lg:text-lg text-sm pt-4'>
               Page views
@@ -60,7 +62,7 @@ const Page = () => {
 
           <div className='bg-white border py-10 px-2'>
             <h2 className='text-[#459231] lg:text-6xl md:text-5xl sm:text-4xl text-3xl font-light'>
-              66%
+              50%
             </h2>
             <p className='text-neutral-700 lg:text-lg text-sm pt-4'>
               Lead generation
@@ -69,7 +71,7 @@ const Page = () => {
 
           <div className='bg-white border py-10 px-2'>
             <h2 className='text-[#459231] lg:text-6xl md:text-5xl sm:text-4xl text-3xl font-light'>
-              10X
+              8X
             </h2>
             <p className='text-neutral-700 lg:text-lg text-sm pt-4'>
               Growth rate
@@ -82,11 +84,11 @@ const Page = () => {
         <div className="pb-[20px] lg:pb-[30px] pt-[20px] lg:pt-[80px]">
 
           <h2 className="lg:text-5xl md:text-4xl text-3xl capitalize font-light text-neutral-800 w-full text-center">
-            Driving Growth for Anthem Senior Living <br className='hidden lg:block' /> <b className="font-bold">through Digital Marketing</b>.
+          Driving Growth for Curb Appeal Lawn<br className='hidden lg:block' /> <b className="font-bold">through Targeted Digital Marketing</b>.
           </h2>
 
           <p className='text-xl pt-4 font-light max-w-3xl mx-auto text-center'>
-            Anthem Senior Living provides personalized, high-quality care for seniors, focusing on enhancing their well-being. Through strategic digital marketing, we targeted relevant audiences, optimized online presence, and utilized content marketing to increase brand awareness and drive growth, ensuring more families find their ideal senior living solution.
+          Curb Appeal Lawn is dedicated to enhancing the beauty of residential and commercial properties. Over the years, Boosted 360 has worked closely with Curb Appeal Lawn to transform their digital presence, drive traffic, and convert leads into long-term clients. Through a strategic blend of SEO and content marketing, we significantly boosted their online visibility and brand recognition.
           </p>
         </div>
 
@@ -99,7 +101,7 @@ const Page = () => {
               Grasping needs:
             </h2>
             <p className='text-neutral-700 font-light text-base pt-5'>
-              Understanding the requirement involves identifying the client&apos;s goals, target audience, and specific needs. It&apos;s crucial to gather detailed insights and expectations to create a tailored, effective digital marketing plan that addresses their objectives.
+            To understand the unique needs of Curb Appeal Lawn, we first identified their business goals and target audience. We analyzed the specific challenges they faced in gaining visibility and attracting high-quality leads. 
             </p>
           </div>
 
@@ -111,7 +113,7 @@ const Page = () => {
               Content marketing:
             </h2>
             <p className='text-neutral-700 font-light text-base pt-5'>
-              Content marketing is the creation and distribution of valuable, relevant content to attract, engage, and retain an audience. It includes blogs, videos, and social media, aiming to drive conversions and build brand authority.
+            Our content marketing strategy for Curb Appeal Lawn involved creating valuable, informative content to engage potential clients. This included blog posts on lawn care tips, videos showing their service quality showcasing before-and-after transformations. 
             </p>
           </div>
 
@@ -123,7 +125,7 @@ const Page = () => {
               Marketing strategy:
             </h2>
             <p className='text-neutral-700 font-light text-base pt-5'>
-              A marketing strategy outlines the plan to achieve specific business goals. It involves market research, identifying target audiences, setting objectives, and selecting appropriate channels, all designed to effectively promote the brand and maximize ROI.
+            The marketing strategy was built around SEO to ensure the website rank high for relevant local keywords. Strategies were focused on user-generated content and client testimonials to create community engagement to convert them into long-term clients.
             </p>
           </div>
 
@@ -135,7 +137,7 @@ const Page = () => {
               Success story:
             </h2>
             <p className='text-neutral-700 font-light text-base pt-5'>
-              A success story in digital marketing showcases a business or campaign that achieved significant results, such as increased engagement or revenue. These stories demonstrate the effectiveness of strategies and build credibility for future efforts.
+            Through our digital marketing efforts, website traffic increased by over 40%, leading to a significant boost in leads. The strategic use of SEO placed them at the forefront of local search results, while content marketing led to higher engagement and increased brand awareness. 
             </p>
           </div>
 
@@ -169,15 +171,11 @@ const Page = () => {
           </p>
 
           <h1>
-            <span>GMB/Google My Business/GMB Marketing</span>
+            <span>Google My Business (GMB) Optimization</span>
           </h1>
 
           <p>
-            We optimized Anthem Senior Living&apos;s Google My Business (GMB) listing to boost local visibility. By ensuring accurate business information, managing reviews, and posting regular updates,
-          </p>
-
-          <p>
-            Anthem Senior Living ranked #1 for key terms such as &quot;Senior care living in Anthem,&quot; &quot;Anthem assisted living,&quot; and &quot;Senior living in Anthem.&quot;
+          We optimized Curb Appeal Lawn&apos;s GMB listing to improve their visibility in local searches. By ensuring their business information was accurate and up-to-date, and by actively managing reviews and posting regular updates, we increased their ranking for keywords like "Lawn care services near me" and "Best lawn service in Simpsonville." This led to greater local exposure and a continuous flow of organic traffic to their site, resulting in a higher number of customer inquiries.
           </p>
 
           <div className='mb-3 '>
@@ -195,7 +193,8 @@ const Page = () => {
 
 
           <h1>Local SEO</h1>
-          <p>We strategically focused on Local SEO to enhance Anthem Senior Living&apos;s search engine ranking. By optimizing their website with location-specific keywords like &quot;Senior care living in Anthem,&quot; &quot;Anthem assisted living,&quot; and &quot;Senior living in Anthem,&quot; we ensured that Anthem Senior Living ranked #1 for these critical terms. This approach significantly boosted their local visibility, driving more targeted traffic from people specifically looking for senior living options in the Anthem area.</p>
+          <p>Focusing on Local SEO, we targeted location-specific keywords such as "Lawn care services Simpsonville and "Affordable lawn care Simpsonville. This tactic enabled Curb Appeal Lawn to rank highly for critical search terms in their service areas. As a result, the company experienced increased local visibility, attracting clients who were actively searching for lawn care services within their geographic region.
+          </p>
           <div className='mb-3 '>
             <Image width={600} height={100} src={'/images/details-image/Antham_google.png'} alt={`Slide`} className='w-full' />
           </div>
@@ -213,7 +212,7 @@ const Page = () => {
           <h1>
             Lead Generation
           </h1>
-          <p>Through a multi-channel approach, we effectively generated leads for Anthem Senior Living via email, phone numbers, social media, and the website&apos;s contact forms. By offering valuable content, engaging calls-to-action, and seamless forms, we made it easy for potential clients to reach out. These efforts captured quality leads, which were nurtured through personalized follow-ups, resulting in increased inquiries and conversions for Anthem Senior Living&apos;s services.</p>
+          <p>We implemented a multi-faceted lead generation strategy for Curb Appeal Lawn, incorporating email marketing, web forms, phone call tracking, and social media inquiries. Our approach involved creating high-quality landing pages, valuable lead magnets like downloadable guides, and compelling calls-to-action. This resulted in an increase in high-quality leads and improved conversion rates, nurturing these leads through personalized email follow-ups and phone consultations.</p>
           <div className='bg-white p-5 mb-3 '>
             <div className='w-[70%] mx-auto'>
               <Image width={600} height={100} src={'/images/details-image/contactform.png'} alt={`Slide`} className='w-full' />
@@ -223,7 +222,7 @@ const Page = () => {
           <h1>
             Content Marketing
           </h1>
-          <p>We implemented a comprehensive content marketing strategy for Anthem Senior Living, focusing on creating informative, engaging, and valuable content for their audience. By producing blog posts, guides, and videos, we educated potential customers on senior care options and assisted living services in Anthem. This approach not only boosted SEO rankings but also helped Anthem Senior Living build trust and long-term relationships with their audience, positioning them as a leader in the senior care industry. </p>
+          <p>We developed a targeted content marketing strategy for Curb Appeal Lawn, which included blogs, service pages, and educational videos that informed potential customers about lawn care best practices and the benefits of professional lawn services. By offering valuable content, we not only increased website traffic but also built trust with the audience, positioning Curb Appeal Lawn as a thought leader in the industry. As a result, Curb Appeal Lawn saw a 300% increase in organic traffic and improved their SEO rankings.</p>
 
           <h1>
             Ad Campaign (PPC/Facebook Ads)
@@ -235,8 +234,7 @@ const Page = () => {
           <h1>
             Landing Page Optimization
           </h1>
-          <p>Optimizing landing pages for conversions was crucial to Anthem Senior Living&apos;s digital marketing success. We focused on refining the design, improving load times, and enhancing call-to-action (CTA) buttons to create an intuitive, user-friendly experience. The addition of relevant imagery and persuasive copy led to higher engagement rates and better conversion of visitors into potential residents, ensuring that each landing page effectively communicated the value of Anthem Senior Living&apos;s services and offerings.</p>
-          <p>It has increased Page Views to 6k/month and 703 visits/day that is an overall 12.5% growth.  </p>
+          <p>Landing page optimization played a critical role in converting visitors into customers. We worked on improving the user experience by optimizing design, reducing load times, and enhancing call-to-action buttons. By incorporating user-friendly features and persuasive copy, we saw higher engagement and conversion rates. This led to more website visitors becoming clients, further boosting Curb Appeal Lawn’s customer base and online presence.</p>
 
         </div>
       </div>
@@ -294,10 +292,10 @@ const Page = () => {
               <Image src={sliderInLogonnjdvd} alt="logo" className="w-[100px] object-contain grayscale" />
             </li>
           </ul> */}
-           <Companies />
+          <Companies />
         </div>
       </div>
-   
+
 
       <WorkWthUsModal isOpen={isModalOpen} onClose={closeModal} />
       <Growing />
